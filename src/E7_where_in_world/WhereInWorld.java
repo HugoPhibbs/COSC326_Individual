8224ea0ff7fe7c174052296e99a694ebab430586
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2022-03-07 19:56:54 +1300

    where in world
    Added first files

---
+++ src/E7_where_in_world/WhereInWorld.java
@@ -0,0 +1,112 @@
+package E7_where_in_world;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+// TODO how to handle incorrect input
+
+public class WhereInWorld {
+
+    ArrayList<String> directions = new ArrayList<String>(Arrays.asList("N", "E", "S", "S"));
+
+    /**
+     * Parses a String input from a user for a place on a map
+     *
+     * @param position String for
+     * @return HashMap<String, String> Containing with keys describing a place on a map
+     */
+    private HashMap<String, String> parseInput(String position) {
+        position = position.strip();
+        String[] splitPosition = position.split(",");
+        assert splitPosition.length == 2;
+        String secondPart = splitPosition[1];
+        String firstPart = splitPosition[0];
+        return null;
+    }
+
+    /**
+     * Splits the first part of a position string into its components
+     *
+     * @param firstPart String for the first part of the position string
+     * @return String array containing the components
+     */
+    private String[] splitFirstPart(String firstPart) {
+        String[] splitStr = firstPart.split(" ");
+        if (directions.contains(splitStr[1])){
+
+        }
+    }
+
+    /**
+     * Checks if a String is numeric or not
+     *
+     * @param str String to be checked
+     * @return boolean as described
+     */
+    private boolean isNumeric(String str){
+        try {
+            Double.parseDouble(str);
+        } catch (NumberFormatException ife) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean firstPartIsValid(String firstPart) {
+        String[] splitStr = firstPart.split(" ");
+        if (splitStr.length == 2){
+            if
+        }
+        if (!(splitStr.length == 1 || splitStr.length ==2)) {
+            return false;
+        }
+    }
+
+
+    private boolean firstPartIsValid(String[] splitFirstPart){
+        
+    }
+    /**
+     * Gets the label from an inputted position string from a user.
+     *
+     * @param position String describing the position of a place on a map
+     * @return String for the label of a position. Null if there is none
+     */
+    public String getLabel(String position) {
+        String[] splitPosition = position.split(" ");
+        String label = splitPosition[splitPosition.length-1];
+        if (!directions.contains(label) & !isNumeric(label)) {
+            return label;
+        }
+        return null;
+    }
+
+    /**
+     * Finds out if a position String has a label or not
+     *
+     * @param position String for the position on a map
+     * @return boolean as described
+     */
+    public boolean hasLabel(String position) {
+        return (getLabel(position) != null);
+    }
+
+    /**
+     * Strips the label off a position String, if it has one.
+     *
+     * @param position String for the position on a map
+     * @return String for inputted position stripped of it's label
+     */
+    public String stripLabel(String position) {
+        if (hasLabel(position)) {
+            int labelLength = getLabel(position).length();
+            position = position.substring(0, position.length()-labelLength);
+        }
+        return position;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
